c910b791e8fc4bfc8ea540e25ff872f82e1edca2
abhinayh56 <kumarabhinay53@gmail.com>
2024-06-04 10:18:01 +0530

    added min and max in SRF

--- SRL/SRL.cpp
+++ SRL/SRL.cpp
@@ -7,25 +7,28 @@ SRL::SRL(){
     bool start = false;
 }
 
-void SRL::init(double dt_, double dx_dt_max_){
-    set_param(dt_, dx_dt_max_);
+void SRL::init(double dt_, double dx_dt_min_, double dx_dt_max_){
+    set_param(dt_, dx_dt_min_, dx_dt_max_);
 }
 
-void SRL::set_param(double dt_, double dx_dt_max_){
+void SRL::set_param(double dt_, double dx_dt_min_, double dx_dt_max_){
     dt = dt_;
+    dx_dt_min = dx_dt_min_;
     dx_dt_max = dx_dt_max_;
 }
 
 double SRL::update(double x_k){
+    double y_k = 0.0;
     if(start==true){
-        double y_k = 0.0;
-        
         start = false;
         y_k = x_k;
     }
     else{
-        if(math_fun.mod(x_k - y_k_1) > dx_dt_max*dt){
-            y_k = y_k_1 + math_fun.sign((x_k - y_k_1))*dx_dt_max*dt;
+        if(x_k > (y_k_1 + dx_dt_max*dt)){
+            y_k = y_k_1 + dx_dt_max*dt;
+        }
+        else if(x_k < (y_k_1 + dx_dt_min*dt)){
+            y_k = y_k_1 + dx_dt_min*dt;
         }
         else{
             y_k = x_k;
@@ -39,12 +42,16 @@ double SRL::update(double x_k){
 
 void SRL::reset(){
     double y_k = 0.0;
-    double y_k_1 = 0;
+    double y_k_1 = 0.0;
     bool start = true;
 }
 
 void SRL::set_dt(double dt_){
     dt = dt_;
+}
+
+void SRL::set_dx_dt_min(double dx_dt_min_){
+    dx_dt_min = dx_dt_min_;
 }
 
 void SRL::set_dx_dt_max(double dx_dt_max_){
@@ -53,6 +60,10 @@ void SRL::set_dx_dt_max(double dx_dt_max_){
 
 double SRL::get_dt(){
     return dt;
+}
+
+double SRL::get_dx_dt_min(){
+    return dx_dt_min;
 }
 
 double SRL::get_dx_dt_max(){
--- SRL/SRL.h
+++ SRL/SRL.h
@@ -1,25 +1,24 @@
 #ifndef FILTER_SRL
 #define FILTER_SRL
 
-#include "Math_functions.h"
-
 class SRL{
     public:
         SRL();
-        void init(double dt_, double dx_dt_max_);
-        void set_param(double dt_, double dx_dt_max_);
+        void init(double dt_, double dx_dt_min_, double dx_dt_max_);
+        void set_param(double dt_, double dx_dt_min_, double dx_dt_max_);
         double update(double x_k);
         void reset();
 
         void set_dt(double dt_);
+        void set_dx_dt_min(double dx_dt_min_);
         void set_dx_dt_max(double dx_dt_max_);
         double get_dt();
+        double get_dx_dt_min();
         double get_dx_dt_max();
 
     private:
-        Math_functions math_fun;
-
         double dt        = 0;
+        double dx_dt_min = 0;
         double dx_dt_max = 0;
 
         double y_k_1     = 0;
